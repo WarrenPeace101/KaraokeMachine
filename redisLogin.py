57d26e83e043311db8c4fbdbe1d2235c887398c1
Warren Mangum <113727042+WarrenPeace101@users.noreply.github.com>
2024-06-10 11:23:37 -0600

    Delete redisLogin.py

--- redisLogin.py
+++
@@ -1,6 +0,0 @@
-REDIS_HOST = 'redis-19588.c60.us-west-1-2.ec2.redns.redis-cloud.com';
-REDIS_PORT = 19588;
-REDIS_PASS = 'yRli4IlKkP0naIym7wqfYbDKwEk3Spaf';
-
-MONGO_USER = 'wdm34'
-MONGO_PASS = 'WMangum11'
